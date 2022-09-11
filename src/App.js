9f89097f91422c974674c38b88f7e937f118eace
Selestine <selemtei@gmail.com>
2022-09-11 19:16:58 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,34 +2,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Service from "./pages/Service/Service";
-import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <div>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/services">
-            <Service />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Home />
         <Footer />
       </Router>
     </div>
